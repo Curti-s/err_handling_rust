b7229e94b8201dce5d1e5a5dd79e49f6429c9ee6
k1r1m1 <kirimi@paygoenergy.co>
2021-04-21 23:44:30 +0300

    matching on different errors

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,21 @@
 // scope by the prelude.
 
 use std::fs::File;
+use std::io::ErrorKind;
 
 #[allow(unused_variables)]
 fn main() {
     let f = File::open("hello.txt");
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.text") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
     };
 }
