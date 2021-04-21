0829074c2f690e9d1114db8d4411a13b2936f258
k1r1m1 <kirimi@paygoenergy.co>
2021-04-21 23:59:52 +0300

    using unwrap as shortcut - if the Result value is Ok variant, unwrap will return the value inside the Ok - if the Result is Err variant, unwrap will call the panic! macro

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,8 @@
 // scope by the prelude.
 
 use std::fs::File;
-use std::io::ErrorKind;
 
 #[allow(unused_variables)]
 fn main() {
-    let f = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file {:?}", error);
-        }
-    });
+    let f = File::open("hello.txt").unwrap();
 }
